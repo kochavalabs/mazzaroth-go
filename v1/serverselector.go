06e4ecb8a2ab57c3acfc2c648c70c94968b7aba3
Juan Miguel Mora <jmora@kochava.com>
2021-04-09 12:10:27 +0200

    Add round robin server selector

---
+++ v1/serverselector.go
@@ -0,0 +1,44 @@
+package mazzaroth
+
+import (
+	"errors"
+	"sync/atomic"
+)
+
+// ServerSelector is the behaviour to select a new server.
+type ServerSelector interface {
+	Peek() string
+}
+
+// RoundRobinServerSelector implements a round robin server selection.
+type RoundRobinServerSelector struct {
+	current    uint64
+	servers    []string
+	numServers int
+}
+
+// NewRoundRobinServerSelector creates a RoundRobinServerSelector object.
+// If the server list is empty the function fails.
+// It follows a minimalistic approach and it doesn't validate the structure of the server elements passed in.
+func NewRoundRobinServerSelector(servers ...string) (*RoundRobinServerSelector, error) {
+	numServers := len(servers)
+
+	if numServers == 0 {
+		return nil, errors.New("could not create the server select with an empty server list")
+	}
+
+	return &RoundRobinServerSelector{
+		current:    0,
+		servers:    servers,
+		numServers: numServers,
+	}, nil
+}
+
+// Peek returs the next server.
+func (rr *RoundRobinServerSelector) Peek() string {
+	atomic.AddUint64(&rr.current, 1)
+
+	n := (rr.current - 1) % uint64(rr.numServers)
+
+	return rr.servers[n]
+}
---
+++ v1/serverselector_test.go
@@ -0,0 +1,28 @@
+package mazzaroth
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestRoundRobinServerSelectorHapyPath checks the correct behaviour of the component.
+func TestRoundRobinServerSelectorHapyPath(t *testing.T) {
+	rr, err := NewRoundRobinServerSelector("a", "b", "c", "d")
+	require.NoError(t, err)
+
+	for i := 0; i < 10; i++ {
+		require.Equal(t, "a", rr.Peek())
+		require.Equal(t, "b", rr.Peek())
+		require.Equal(t, "c", rr.Peek())
+		require.Equal(t, "d", rr.Peek())
+	}
+}
+
+// TestRoundRobinServerDetectsEmptyServerList checks the constructor function returns an error when an empty list is passed in.
+func TestRoundRobinServerDetectsEmptyServerList(t *testing.T) {
+	rr, err := NewRoundRobinServerSelector()
+	require.Error(t, err)
+	require.Equal(t, "could not create the server select with an empty server list", err.Error())
+	require.Nil(t, rr)
+}
