52bd9512408c6d0caf4fccbcbb094398afda2173
Ethan Lewis <ethanlewis787@gmail.com>
2021-12-07 10:21:33 -0800

    refactor: reworked client impl

--- client.go
+++ client.go
@@ -1,20 +1,24 @@
 package mazzaroth
 
-import "github.com/kochavalabs/mazzaroth-xdr/xdr"
+import (
+	"context"
+
+	"github.com/kochavalabs/mazzaroth-xdr/xdr"
+)
 
 // Client defines a mazzaroth client that exposes common
 // endpoints exposed by a mazzaroth readonly node.
 type Client interface {
-	AccountLookup(channelID string, accountID string) (*xdr.Account, error)
-	AuthorizationLookup(channelID string, accountID string) (*xdr.Authorization, error)
-	BlockHeaderLookup(channelID string, blockID string) (*xdr.BlockHeader, error)
-	BlockHeaderList(channelID string, blockHeight int, number int) (*[]xdr.BlockHeader, error)
-	BlockHeight(channelID string) (*xdr.BlockHeight, error)
-	BlockLookup(channelID string, blockID string) (*xdr.Block, error)
-	BlockList(channelID string, blockHeight int, number int) (*[]xdr.Block, error)
-	ChannelAbi(channelID string) (*xdr.Abi, error)
-	ChannelLookup(channelID string) (*xdr.Config, error)
-	ReceiptLookup(channelID string, transactionID string) (*xdr.Receipt, error)
-	TransactionLookup(channelID string, transactionID string) (*xdr.Transaction, error)
-	TransactionSubmit(transaction *xdr.Transaction) (*xdr.ID, *xdr.Receipt, error)
+	AccountLookup(ctx context.Context, channelID string, accountID string) (*xdr.Account, error)
+	AuthorizationLookup(ctx context.Context, channelID string, accountID string) (*xdr.Authorization, error)
+	BlockHeaderLookup(ctx context.Context, channelID string, blockID string) (*xdr.BlockHeader, error)
+	BlockHeaderList(ctx context.Context, channelID string, blockHeight int, number int) (*[]xdr.BlockHeader, error)
+	BlockHeight(ctx context.Context, channelID string) (*xdr.BlockHeight, error)
+	BlockLookup(ctx context.Context, channelID string, blockID string) (*xdr.Block, error)
+	BlockList(ctx context.Context, channelID string, blockHeight int, number int) (*[]xdr.Block, error)
+	ChannelAbi(ctx context.Context, channelID string) (*xdr.Abi, error)
+	ChannelLookup(ctx context.Context, channelID string) (*xdr.Config, error)
+	ReceiptLookup(ctx context.Context, channelID string, transactionID string) (*xdr.Receipt, error)
+	TransactionLookup(ctx context.Context, channelID string, transactionID string) (*xdr.Transaction, error)
+	TransactionSubmit(ctx context.Context, transaction *xdr.Transaction) (*xdr.ID, *xdr.Receipt, error)
 }
--- clientimpl.go
+++ clientimpl.go
@@ -3,12 +3,10 @@ package mazzaroth
 import (
 	"bytes"
 	"context"
-	"crypto/ed25519"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 
 	"github.com/kochavalabs/mazzaroth-xdr/xdr"
@@ -40,8 +38,157 @@ func NewMazzarothClient(servers []string, options ...Options) (*ClientImpl, erro
 	}, nil
 }
 
+// AccountLookup calls the endpoint: /v1/channels/{channel_id}/accounts/{account_id}.
+func (c *ClientImpl) AccountLookup(ctx context.Context, channelID string, accountID string) (*xdr.Account, error) {
+
+	url := fmt.Sprintf("%s/%s/channels/%s/accounts/%s", c.address, version, channelID, accountID)
+
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to handle http response")
+	}
+
+	if xdrResp.Account != nil {
+		return xdrResp.Account, nil
+	}
+
+	return nil, errors.New("Missing account")
+}
+
+func (c *ClientImpl) AuthorizationLookup(ctx context.Context, channelID string, accountID string) (*xdr.Authorization, error) {
+	return nil, nil
+}
+
+// BlockHeight calls the endpoint: /v1/channels/{channel_id}/blocks/height.
+func (c *ClientImpl) BlockHeight(ctx context.Context, channelID string) (*xdr.BlockHeight, error) {
+	url := fmt.Sprintf("%s/%s/channels/%s/blocks/height", c.address, version, channelID)
+
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to handle http response")
+	}
+
+	if xdrResp.Height != nil {
+		return xdrResp.Height, nil
+	}
+
+	return nil, errors.New("Missing block height")
+}
+
+// BlockLookup calls the endpoint: /v1/channels/{channel_id}/blocks/{id}.
+func (c *ClientImpl) BlockLookup(ctx context.Context, channelID, blockID string) (*xdr.Block, error) {
+	url := fmt.Sprintf("%s/%s/channels/%s/blocks/%s", c.address, version, channelID, blockID)
+
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to handle http response")
+	}
+
+	if xdrResp.Block != nil {
+		return xdrResp.Block, nil
+	}
+
+	return nil, errors.New("Missing block")
+}
+
+// BlockList calls the endpoint: /v1/channels/{channel_id}/blocks?{number,height}.
+func (c *ClientImpl) BlockList(ctx context.Context, channelID string, blockHeight int, number int) (*[]xdr.Block, error) {
+	url := fmt.Sprintf("%s/%s/channels/%s/blocks?height=%d&number=%d", c.address, version, channelID, blockHeight, number)
+
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to handle http response")
+	}
+
+	if xdrResp.Blocks != nil {
+		return xdrResp.Blocks, nil
+	}
+
+	return nil, errors.New("Missing blocks")
+}
+
+// BlockHeaderLookup calls the endpoint: /v1/channels/{channel_id}/blockheaders/{id}.
+func (c *ClientImpl) BlockHeaderLookup(ctx context.Context, channelID, blockID string) (*xdr.BlockHeader, error) {
+	url := fmt.Sprintf("%s/%s/channels/%s/blockheaders/%s", c.address, version, channelID, blockID)
+
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to handle http response")
+	}
+
+	if xdrResp.BlockHeader != nil {
+		return xdrResp.BlockHeader, nil
+	}
+
+	return nil, errors.New("Mising blockheader")
+}
+
+// BlockHeaderList calls the endpoint: /v1/channels/{channel_id}/blockheaders?{blockHeight,number}.
+func (c *ClientImpl) BlockHeaderList(ctx context.Context, channelID string, blockHeight int, number int) (*[]xdr.BlockHeader, error) {
+	url := fmt.Sprintf("%s/%s/channels/%s/blockheaders?height=%d&number=%d", c.address, version, channelID, blockHeight, number)
+
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to handle http response")
+	}
+
+	if xdrResp.BlockHeaders != nil {
+		return xdrResp.BlockHeaders, nil
+	}
+
+	return nil, errors.New("Missing blockHeaders")
+}
+
+// ChannelLookup calls the endpoint: /v1/channels/{channel_id}.
+func (c *ClientImpl) ChannelLookup(ctx context.Context, channelID string) (*xdr.Config, error) {
+	url := fmt.Sprintf("%s/%s/channels/%s", c.address, version, channelID)
+
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to handle http response")
+	}
+
+	if xdrResp.Config != nil {
+		return xdrResp.Config, nil
+	}
+
+	return nil, errors.New("Missing channel config")
+}
+
+// ChannelAbi calls the endpoint: /v1/channels/{channel_id}/abi.
+func (c *ClientImpl) ChannelAbi(ctx context.Context, channelID string) (*xdr.Abi, error) {
+	url := fmt.Sprintf("%s/%s/channels/%s/abi", c.address, version, channelID)
+
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to handle http response")
+	}
+
+	if xdrResp.Abi != nil {
+		return xdrResp.Abi, nil
+	}
+
+	return nil, errors.New("Missing channel abi")
+}
+
+// ReceiptLookup calls the endpoint: /v1/channels/{channel_id}/receipts/{id}.
+func (c *ClientImpl) ReceiptLookup(ctx context.Context, channelID, transactionID string) (*xdr.Receipt, error) {
+	url := fmt.Sprintf("%s/%s/channels/%s/receipts/%s", c.address, version, channelID, transactionID)
+
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to handle http response")
+	}
+
+	if xdrResp.Receipt != nil {
+		return xdrResp.Receipt, nil
+	}
+
+	return nil, errors.New("Missing receipt")
+}
+
 // TransactionSubmit calls the endpoint: /v1/channels/{channel_id}/transactions.
-func (c *ClientImpl) TransactionSubmit(transaction *xdr.Transaction) (*xdr.ID, *xdr.Receipt, error) {
+func (c *ClientImpl) TransactionSubmit(ctx context.Context, transaction *xdr.Transaction) (*xdr.ID, *xdr.Receipt, error) {
 	b, err := json.Marshal(transaction)
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "unable to marshal to json")
@@ -54,191 +201,61 @@ func (c *ClientImpl) TransactionSubmit(transaction *xdr.Transaction) (*xdr.ID, *
 
 	url := fmt.Sprintf("%s/%s/channels/%s/transactions", c.address, version, channelID)
 
-	response, err := makeRequest(c.httpClient, http.MethodPost, url, bytes.NewReader(b))
+	xdrResp, err := c.do(ctx, url, http.MethodPost, b)
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "unable to make a request to transaction submit endpoint")
 	}
 
-	return nil, response.Receipt, nil
+	if xdrResp.TransactionID != nil {
+		return xdrResp.TransactionID, nil, nil
+	} else if xdrResp.Receipt != nil {
+		return &xdrResp.Receipt.TransactionID, xdrResp.Receipt, nil
+	}
+	return nil, nil, errors.New("unable to processes transaction")
 }
 
 // TransactionLookup calls the endpoint: /v1/channels/{channel_id}/transactions/{id}.
-func (c *ClientImpl) TransactionLookup(channelID string, transactionID string) (*xdr.Transaction, error) {
+func (c *ClientImpl) TransactionLookup(ctx context.Context, channelID string, transactionID string) (*xdr.Transaction, error) {
 	url := fmt.Sprintf("%s/%s/channels/%s/transactions/%s", c.address, version, channelID, transactionID)
 
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
+	xdrResp, err := c.do(ctx, url, http.MethodGet, nil)
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to transaction lookup endpoint")
+		return nil, errors.Wrap(err, "unable to handle http response")
 	}
 
-	return response.Transaction, nil
+	if xdrResp.Transaction != nil {
+		return xdrResp.Transaction, nil
+	}
+
+	return nil, errors.New("missing transaction")
 }
 
-// ReceiptLookup calls the endpoint: /v1/channels/{channel_id}/receipts/{id}.
-func (c *ClientImpl) ReceiptLookup(channelID, transactionID string) (*xdr.Receipt, error) {
-	url := fmt.Sprintf("%s/%s/channels/%s/receipts/%s", c.address, version, channelID, transactionID)
+func (c *ClientImpl) do(ctx context.Context, url string, method string, body []byte) (*xdr.Response, error) {
 
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to receipts lookup endpoint")
-	}
-
-	return response.Receipt, nil
-}
-
-// BlockLookup calls the endpoint: /v1/channels/{channel_id}/blocks/{id}.
-func (c *ClientImpl) BlockLookup(channelID, blockID string) (*xdr.Block, error) {
-	url := fmt.Sprintf("%s/%s/channels/%s/blocks/%s", c.address, version, channelID, blockID)
-
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to block lookup endpoint")
-	}
-
-	return response.Block, nil
-}
-
-// BlockList calls the endpoint: /v1/channels/{channel_id}/blocks?{number,height}.
-func (c *ClientImpl) BlockList(channelID string, blockHeight int, number int) (*[]xdr.Block, error) {
-	url := fmt.Sprintf("%s/%s/channels/%s/blocks?height=%d&number=%d", c.address, version, channelID, blockHeight, number)
-
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to block by height lookup endpoint")
-	}
-
-	return response.Blocks, nil
-}
-
-// BlockHeaderLookup calls the endpoint: /v1/channels/{channel_id}/blockheaders/{id}.
-func (c *ClientImpl) BlockHeaderLookup(channelID, blockID string) (*xdr.BlockHeader, error) {
-	url := fmt.Sprintf("%s/%s/channels/%s/blockheaders/%s", c.address, version, channelID, blockID)
-
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to blockheaders lookup endpoint")
-	}
-
-	return response.BlockHeader, nil
-}
-
-// BlockHeaderList calls the endpoint: /v1/channels/{channel_id}/blockheaders?{blockHeight,number}.
-func (c *ClientImpl) BlockHeaderList(channelID string, blockHeight int, number int) (*[]xdr.BlockHeader, error) {
-	url := fmt.Sprintf("%s/%s/channels/%s/blockheaders?height=%d&number=%d", c.address, version, channelID, blockHeight, number)
-
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to blockheaders by blockheight lookup endpoint")
-	}
-
-	return response.BlockHeaders, nil
-}
-
-// ChannelLookup calls the endpoint: /v1/channels/{channel_id}.
-func (c *ClientImpl) ChannelLookup(channelID string) (*xdr.Config, error) {
-	url := fmt.Sprintf("%s/%s/channels/%s", c.address, version, channelID)
-
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to channel lookup endpoint")
-	}
-
-	return response.Config, nil
-}
-
-// ChannelAbi calls the endpoint: /v1/channels/{channel_id}/abi.
-func (c *ClientImpl) ChannelAbi(channelID string) (*xdr.Abi, error) {
-	url := fmt.Sprintf("%s/%s/channels/%s/abi", c.address, version, channelID)
-
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to channel lookup endpoint")
-	}
-
-	return response.Abi, nil
-}
-
-// BlockHeight calls the endpoint: /v1/channels/{channel_id}/blocks/height.
-func (c *ClientImpl) BlockHeight(channelID string) (*xdr.BlockHeight, error) {
-	url := fmt.Sprintf("%s/%s/channels/%s/blocks/height", c.address, version, channelID)
-
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to channel lookup endpoint")
-	}
-
-	return response.Height, nil
-}
-
-// AccountLookup calls the endpoint: /v1/channels/{channel_id}/accounts/{accout_id}.
-func (c *ClientImpl) AccountLookup(channelID string, seed string) (*xdr.Account, error) {
-	seedBin, err := hex.DecodeString(seed)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to decode seed")
-	}
-	privateKey := ed25519.NewKeyFromSeed(seedBin)
-	address, err := xdr.IDFromPublicKey(privateKey.Public())
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to create private key")
-	}
-
-	url := fmt.Sprintf("%s/%s/channels/%s/accounts/%s", c.address, version, channelID, hex.EncodeToString(address[:]))
-
-	response, err := makeRequest(c.httpClient, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to make a request to channel lookup endpoint")
-	}
-
-	return response.Account, nil
-}
-
-func (c *ClientImpl) AuthorizationLookup(channelID string, accountID string) (*xdr.Authorization, error) {
-	return nil, nil
-}
-
-func makeRequest(httpClient *http.Client, method, url string, body io.Reader) (*xdr.Response, error) {
-
-	req, err := http.NewRequestWithContext(context.Background(), method, url, body)
+	req, err := http.NewRequestWithContext(context.Background(), method, url, bytes.NewReader(body))
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to create a new request")
 	}
 
-	resp, err := httpClient.Do(req)
+	response, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to make http request")
 	}
-	defer func() {
-		err := resp.Body.Close()
-		if err != nil {
-			log.Println(err)
-		}
-	}()
 
-	switch resp.StatusCode {
-	case http.StatusOK:
-	case http.StatusNotFound:
-		return nil, ErrNotFound
-	case http.StatusInternalServerError:
-		return nil, ErrInternalServer
-	default:
-		responseBody, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, errors.Wrap(err, "could not read the body")
-		}
-
-		return nil, fmt.Errorf("http status %d - %s", resp.StatusCode, string(responseBody))
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request failed with status %d", response.StatusCode)
 	}
 
-	responseBody, err := io.ReadAll(resp.Body)
+	responseBody, err := io.ReadAll(response.Body)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not read the body")
 	}
 
-	responseXDR := xdr.Response{}
-	err = responseXDR.UnmarshalJSON(responseBody)
+	xdrResp := xdr.Response{}
+	err = xdrResp.UnmarshalJSON(responseBody)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not unmarshal the body")
 	}
 
-	return &responseXDR, nil
+	return &xdrResp, nil
 }
