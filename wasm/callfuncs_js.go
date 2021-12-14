c4b288c2ef0cfc0abaefffff4ee114d092df3af5
Ethan Lewis <ethanlewis787@gmail.com>
2021-12-14 10:41:19 -0800

    feat: add client wrapper js funcs

--- wasm/callfuncs_js.go
+++ wasm/callfuncs_js.go
@@ -4,6 +4,7 @@ import (
 	"syscall/js"
 
 	"github.com/kochavalabs/mazzaroth-go"
+	"github.com/kochavalabs/mazzaroth-xdr/xdr"
 )
 
 func callFunction(callBuilder *mazzaroth.CallBuilder) js.Func {
@@ -16,7 +17,17 @@ func callFunction(callBuilder *mazzaroth.CallBuilder) js.Func {
 }
 
 func callArguments(callBuilder *mazzaroth.CallBuilder) js.Func {
+	// dev note:: we are expecting args to be a string and that the caller
+	// will pass multiple string args i.e func("x","y","z")
+	// Alternatively we could construct a json array and pass a
+	// json string to this function and unmarshal it into a []xdr.Agruments
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		arguments := make([]xdr.Argument, 0, 0)
+		for _, arg := range args {
+			a := xdr.Argument(arg.String())
+			arguments = append(arguments, a)
+		}
+		callBuilder.Arguments(arguments...)
 		return map[string]interface{}{
 			"Sign": sign(callBuilder),
 		}
--- wasm/configfuncs_js.go
+++ wasm/configfuncs_js.go
@@ -21,6 +21,10 @@ func configOwner(configBuilder *mazzaroth.ConfigBuilder) js.Func {
 }
 
 func configAdmins(configBuilder *mazzaroth.ConfigBuilder) js.Func {
+	// dev note:: we are expecting args to be a string and that the caller
+	// will pass multiple string args i.e func("x","y","z")
+	// Alternatively we could construct a json array and pass a
+	// json string to this function and unmarshal it into a []xdr.ID
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		ids := make([]xdr.ID, 0, 0)
 		for _, admin := range args {
