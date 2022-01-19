7aa5fa818749437b1f5f1e8864c8e89956e3acf1
Ethan Lewis <ethanlewis787@gmail.com>
2022-01-18 16:16:16 -0800

    fix: add rand.Seed call to generate random uint64

--- gennonce.go
+++ gennonce.go
@@ -1,7 +1,17 @@
 package mazzaroth
 
-import "math/rand"
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func seedRand() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func GenerateNonce() uint64 {
+	var once sync.Once
+	once.Do(seedRand)
 	return rand.Uint64()
 }
